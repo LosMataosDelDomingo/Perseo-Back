6245d42bd16526d17cad59f855ceef6585e1e2e1
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-01-23 10:57:04 +0100

    Added constants

---
+++ src/constants/env.ts
@@ -0,0 +1,8 @@
+export default {
+    jwtSecret: process.env.JWT_SECRET || "randomtoken",
+    DB: {
+        URI: process.env.MONGODB_URI || "mongodb://localhost/apinuwe",
+        USER: process.env.MONGODB_USER,
+        PASSWORD: process.env.MONGODB_PASSWORD
+    }
+}
