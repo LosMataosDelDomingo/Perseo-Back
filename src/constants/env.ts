f7e3be879cf474c06c5d7ef9c4bc13c4025638ee
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-01-23 18:52:46 +0100

    env updated

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -4,7 +4,5 @@ export default {
     jwtSecret: process.env.JWT_SECRET || "randomtoken",
     DB: {
         URI: `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@perseocluster.gakol.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,
-        USER: process.env.MONGODB_USER || "root",
-        PASSWORD: process.env.MONGODB_PASSWORD || "pwd"
     }
 }
