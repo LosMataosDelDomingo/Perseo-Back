aff06eaae14772ad553e9c2a3a5e938770591f4d
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-01-23 11:43:53 +0100

    Updated middleware: added trycatch

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -12,18 +12,17 @@ export const verifyNewUser = (req: Request, res: Response, next: NextFunction) =
     try {
 
         const { user, education, work }: INewUser = req.body;
+
+
         const { name, surname, email, phone, birthAge, address, summary } = user;
-
-
-        if (typeof name == 'undefined' || typeof surname == 'undefined' || typeof email == 'undefined' || typeof phone == 'undefined')
+        if (typeof name == 'undefined' || typeof surname == 'undefined' || typeof email == 'undefined' || typeof phone == 'undefined' || typeof birthAge == 'undefined')
             return res.status(400).json({ msg: DATA_MISSING });
 
 
         education.forEach(educationData => {
-            const { dateStart, dateEnd, educationCenter, discipline } = educationData
-            if (typeof dateStart == 'undefined' || typeof dateEnd == 'undefined' || typeof educationCenter == 'undefined' || typeof discipline == 'undefined')
+            const { dateStart, dateEnd, title, educationCenter, discipline } = educationData
+            if (typeof dateStart == 'undefined' || typeof dateEnd == 'undefined' || typeof title == 'undefined' || typeof educationCenter == 'undefined' || typeof discipline == 'undefined')
                 throw new Error(DATA_MISSING);
-
         });
 
 
@@ -36,10 +35,6 @@ export const verifyNewUser = (req: Request, res: Response, next: NextFunction) =
     } catch (e) {
         return res.status(400).json({ msg: e.message });
     }
-
-
-    // if (!(email.trim() && emailValidator(email) && password.trim() && telf.trim()))
-    //     return res.status(400).json({ msg: err.SIGNUP_DATA_ERROR });
 
     next();
 };
