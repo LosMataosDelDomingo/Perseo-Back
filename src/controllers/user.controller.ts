292ea87f4e3f2b50b0178f0b9d87bc445212fb8f
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-01-23 11:44:33 +0100

    New user controller modified

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,22 +1,34 @@
 import { Request, Response } from "express";
 import User from "../models/user.model";
 import Education from "../models/education.model";
+import Work from "../models/work_experience.model"
 import { IUser } from './../models/user.model';
 import { INewUser } from './../interfaces/INewUser.interface';
 import { IEducation } from './../models/education.model';
 
 export const createNewUser = async (req: Request, res: Response): Promise<Response> => {
-    const {user, education, work}: INewUser = req.body;
-    
-    
-    // education.forEach(()=> console.log("ds"))
-    // const e = new Education(education[0])
-    const newUser = new User(user);
-    console.log( newUser);
-    // const user = new User(body)
-    // user.save()
+    try {
+        const { user, education, work }: INewUser = req.body;
 
+        const newUser = new User(user);
+        await newUser.save()
 
+        education.forEach( educationData => {
+            const newEducation = new Education(educationData)
+            newEducation.userID = newUser._id
+            newEducation.save()
+            console.log(newEducation);            
+        });
 
-    return res.status(200).json({ res: "data" });
+        work.forEach( workData => {
+            const newWork = new Work(workData)
+            newWork.userID = newUser._id
+            newWork.save()
+            console.log(newWork);            
+        });
+
+        return res.status(200).json({ res: "data" });
+    } catch (e) {
+        return res.status(400).json({ msg: e.message });
+    }
 }
