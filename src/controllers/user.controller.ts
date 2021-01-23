e65104e40ceddb7f79af9a1040bf791dd98aaf14
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-01-23 13:38:53 +0100

    GetAllUsers Controller Added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,29 +1,42 @@
+// ** Controller for User Model **
+
 import { Request, Response } from "express";
-import User from "../models/user.model";
 import Education from "../models/education.model";
 import Work from "../models/work_experience.model"
 import { IUser } from './../models/user.model';
 import { INewUser } from './../interfaces/INewUser.interface';
-import { IEducation } from './../models/education.model';
+import { db_newUser } from './../repositories/user.repository';
+import { serv_getAllUsersData } from "../services/user.service";
 
+
+export const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const allUsers = await serv_getAllUsersData()
+        
+        return res.status(200).json({ res: allUsers });
+    } catch (e) {
+        return res.status(400).json({ msg: e.message });
+    }
+}
+
+// New User: Save personal data, education and work experience from the new user
 export const createNewUser = async (req: Request, res: Response): Promise<Response> => {
     try {
         const { user, education, work }: INewUser = req.body;
 
-        const newUser = new User(user);
-        await newUser.save()
+        const newUser: IUser = await db_newUser(user)
 
-        education.forEach( educationData => {
+        education.forEach( async educationData => {
             const newEducation = new Education(educationData)
             newEducation.userID = newUser._id
-            newEducation.save()
+            await newEducation.save()
             console.log(newEducation);            
         });
 
-        work.forEach( workData => {
+        work.forEach( async workData => {
             const newWork = new Work(workData)
             newWork.userID = newUser._id
-            newWork.save()
+            await newWork.save()
             console.log(newWork);            
         });
 
