135564f1e30b69c30d5a6610953bb73ad2f35e52
Erix Mamani Villacresis <erixmv022@gmail.com>
2021-01-24 11:22:15 +0100

    minor error on JWT Token

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,7 +1,7 @@
 // ** Enviroment variables **
 
 export default {
-    jwtSecret: process.env.JWT_SECRET || "randomtoken",
+    JWT_SECRET: process.env.JWT_SECRET || "randomtoken",
     DB: {
         URI: `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@perseocluster.gakol.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,
     }
